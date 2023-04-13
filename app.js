09d2ec0f42e25d3341d6e30f311c8fd8398e662f
Teodora <teodoraharabor7@gmail.com>
2023-04-13 14:57:37 +0300

    submitMsg works

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-document.getElementById("submit").addEventListener("click", () => {
+let submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", () => {
   document.getElementById("submitMsg").classList.remove("hidden");
   setTimeout(() => {
     document.getElementById("submitMsg").classList.add("hidden");
   }, 2000);
+  console.log("submit buton" + submitBtn);
 });
